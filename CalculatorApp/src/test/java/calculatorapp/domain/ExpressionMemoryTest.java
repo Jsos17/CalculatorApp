229b97a6399d204c0b7ef8f3c8c1b0c49b74ee4e
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-04-17 06:54:07 +0300

    memory test class

---
+++ CalculatorApp/src/test/java/calculatorapp/domain/ExpressionMemoryTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorapp.domain;
+
+import javafx.collections.ObservableList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jpssilve
+ */
+public class ExpressionMemoryTest {
+    private CalculatorService calc;
+    private InputParser inParser;
+    private ExpressionMemory exprMem;
+   
+    @Before
+    public void setUp() {
+        inParser = new InputParser(calc);
+        exprMem = new ExpressionMemory(inParser, 10);
+    }
+    
+    @Test
+    public void setMemoryWorks() {
+        exprMem.setMemoryLimit(20);
+        assertEquals(20, exprMem.getMemoryLimit());
+    }
+    
+}
